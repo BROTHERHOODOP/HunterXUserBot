013e93652f9e499f5b69965648f12580a4da5e4a
ᴍʀ.ᴘʀᴏɢᴀᴍᴍᴇʀ <109902449+MrProgrammer72@users.noreply.github.com>
2022-10-30 19:00:01 +0530

    Update bot_alive.py

--- hunterx/plugins/assistant/bot_alive.py
+++ hunterx/plugins/assistant/bot_alive.py
@@ -1,9 +1,9 @@
 from telethon import events
 
-from userbot import ALIVE_NAME, bot
+from hunterx import ALIVE_NAME, bot
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://telegra.ph/file/22e70478bd48568451a1f.jpg"
+PM_IMG = "https://te.legra.ph/file/8356516a7f90416c3a2ce.jpg"
 pm_caption = "➥ **ASSISTANT IS:** `ONLINE`\n\n"
 pm_caption += "➥ **SYSTEMS STATS**\n"
 pm_caption += "➥ **Telethon Version:** `1.15.0` \n"
@@ -14,8 +14,8 @@ pm_caption += f"➥ **Version** : `2.0`\n"
 pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
 pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
 pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/DARK-COBRA/DARKCOBRA/blob/master/LICENSE)\n"
-pm_caption += "➥ **Copyright** : By [DarkCobra](https://t.me/Dark_cobra_support_group)\n"
-pm_caption += "[Assistant By DarkCobra](https://t.me/Dark_cobra_support_group)"
+pm_caption += "➥ **Copyright** : By [HunterXUserBot](https://t.me/HunterXUserBot_support)\n"
+pm_caption += "[Assistant By HunterXUserBot](https://t.me/HunterXUserBot_support)"
 
 # only Owner Can Use it
 @tgbot.on(events.NewMessage(pattern="^/alive", func=lambda e: e.sender_id == bot.uid))
