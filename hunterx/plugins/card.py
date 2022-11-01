90e7cbc84bf8765096afa0901727fb98c79b51a8
ᴍʀ.ᴘʀᴏɢᴀᴍᴍᴇʀ <109902449+MrProgrammer72@users.noreply.github.com>
2022-11-01 08:26:51 +0530

    Update card.py

--- hunterx/plugins/card.py
+++ hunterx/plugins/card.py
@@ -1,13 +1,13 @@
 # Original By 
-# @THE_B_LACK_HAT
-# @danish_00
-# For #Cobra
+# 
+# 
+# For #hunterx
 #
 # Card Generator
 ##############################
 from faker import Faker as dc
-from userbot.utils import admin_cmd as hehe
-from userbot import bot as cobra
+from hunterx.utils import admin_cmd as hehe
+from hunterx import bot as cobra
 @cobra.on(hehe("card"))
 async def _cobra(dark):
     cyber = dc()
