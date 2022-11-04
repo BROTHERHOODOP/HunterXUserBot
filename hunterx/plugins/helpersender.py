dda1da11b87e87426263705d7ef46fbbbdd6f651
Spydyrkt <112763136+Spydyrkt@users.noreply.github.com>
2022-11-04 11:32:16 +0530

    Update helpersender.py

--- hunterx/plugins/helpersender.py
+++ hunterx/plugins/helpersender.py
@@ -1,24 +1,24 @@
-from userbot import  *
+from hunterx import  *
 from telethon import events
-from userbot.events import *
+from hunterx.events import *
 from telethon import functions, types
 from telethon.tl.types import InputMessagesFilterDocument
-from userbot.utils import command, remove_plugin, load_module
+from hunterx.utils import command, remove_plugin, load_module
 from pathlib import Path
-from userbot import LOAD_PLUG
+from hunterx import LOAD_PLUG
 from datetime import datetime
 DELETE_TIMEOUT = 5
 import sys, asyncio, traceback, os, importlib
 import userbot.utils
-from userbot.utils import *
-from userbot import CMD_HELP
+from hunterx.utils import *
+from hunterx import CMD_HELP
 @borg.on(admin_cmd(pattern="sqlsend (?P<shortname>\w+)$"))
 async def send(event):
     if event.fwd_from:
         return
     message_id = event.message.id
     input_str = event.pattern_match["shortname"]
-    the_plugin_file = "./userbot/helper//{}.py".format(input_str)
+    the_plugin_file = "./hunterx/helper//{}.py".format(input_str)
     start = datetime.now()
     await event.client.send_file(  # pylint:disable=E0602
         event.chat_id,
