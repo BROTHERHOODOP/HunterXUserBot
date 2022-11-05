c169a1308ca120e62c4d63f080d112f7d9466bcf
Spydyrkt <112763136+Spydyrkt@users.noreply.github.com>
2022-11-05 14:21:17 +0530

    Update feks.py

--- hunterx/plugins/feks.py
+++ hunterx/plugins/feks.py
@@ -2,10 +2,10 @@ import asyncio
 from telethon import events
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import ChannelParticipantsAdmins
-from userbot.utils import admin_cmd
+from hunterx.utils import admin_cmd
 import random, re
 import asyncio
-from userbot import CMD_HELP
+from hunterx import CMD_HELP
 
 
 
