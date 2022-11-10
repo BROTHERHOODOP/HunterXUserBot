f60bfceab50788170e95e4e3c18cb327a3b71d44
ᴍʀ.ᴘʀᴏɢᴀᴍᴍᴇʀ <109902449+MrProgrammer72@users.noreply.github.com>
2022-11-10 21:14:17 +0530

    Update vss.py

--- hunterx/plugins/vss.py
+++ hunterx/plugins/vss.py
@@ -9,9 +9,9 @@ import os
 import time
 
 from telethon.tl.types import DocumentAttributeFilename
-from userbot import CMD_HELP, bot
-from userbot.events import register
-from userbot.utils import progress
+from hunterx import CMD_HELP, bot
+from hunterx.events import register
+from hunterx.utils import progress
 
 
 @register(outgoing=True, pattern=r"^\.ssvideo(?: |$)(.*)")
