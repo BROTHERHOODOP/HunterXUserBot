4852ac71ac2e33455d00bdbc7ee6cdfa426cf33b
Spyder~RKT <112763136+Spydyrkt@users.noreply.github.com>
2022-11-16 11:02:42 +0530

    Update locks.py

--- hunterx/plugins/locks.py
+++ hunterx/plugins/locks.py
@@ -1,9 +1,9 @@
 from telethon.tl.functions.messages import EditChatDefaultBannedRightsRequest
 from telethon.tl.types import ChatBannedRights
-from userbot import ALIVE_NAME
-from userbot import CMD_HELP
-from userbot.events import register, errors_handler
-from userbot.utils import admin_cmd
+from hunterx import ALIVE_NAME
+from hunterx import CMD_HELP
+from hunterx.events import register, errors_handler
+from hunterx.utils import admin_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
 #@register(outgoing=True, pattern=r"^.lock ?(.*)")
