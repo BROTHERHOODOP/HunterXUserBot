337cac13879ea15304b368ae7cb9d925b13ed432
Spyder~RKT <112763136+Spydyrkt@users.noreply.github.com>
2022-11-16 09:52:30 +0530

    Update telegraph.py

--- hunterx/plugins/telegraph.py
+++ hunterx/plugins/telegraph.py
@@ -4,8 +4,8 @@ import os
 from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
-from userbot.utils import admin_cmd
-from userbot import CMD_HELP
+from hunterx.utils import admin_cmd
+from hunterx import CMD_HELP
 
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=Config.TELEGRAPH_SHORT_NAME)
